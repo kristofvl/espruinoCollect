76433c3280373c9b3cb4ddc27f8a2bbd634f2fb2
Kristof Van Laerhoven <kryztov@gmail.com>
2024-02-14 13:24:00 +0100

    Update plotData.py

--- base/plotData.py
+++ base/plotData.py
@@ -1,25 +1,39 @@
-# plot Espruino Data from CSV:
-def plotCSV():
+import matplotlib.pyplot as plt
+import csv
+
+# plot from CSV:
+def plotCSV(logFileName):
     ts = [str(x) for x in range(0, 24*4)]
     temp = [x for x in range(0, 24*4)]
     lght = [x for x in range(0, 24*4)]
     date = ""
-    with open('log.csv', 'r') as f:
-        csv_reader = csv.reader(f)
-        for i, line in enumerate(csv_reader, 0):
-            j = i % 96
-            if len(line)==4:
-                if date == line[0]:
-                     ts[j] = line[1];
-                     temp[j] = float(line[2]); lght[j] = float(line[3])
-                else:
-                     ts[j] = line[1];
-                     temp[j] = float(line[2]); lght[j] = float(line[3])
-                     date = line[0];
-                     if i > (24*4):
-                        plt.plot(ts, temp, label=date)
-                        plt.xticks(ts[::8])
-                        plt.tick_params(axis='x', labelrotation=90)
-    plt.legend()
-    plt.show()
-  
+    try:
+        file = open(logFileName)
+    except FileNotFoundError:
+        print('Error: '+file.name+' not found')
+    else:
+        with file:
+            csv_reader = csv.reader(file)
+            print('reading '+file.name)
+            for i, line in enumerate(csv_reader, 0):
+                j = i % 96
+                if len(line)==4:
+                    ts[j] = line[1];
+                    temp[j] = float(line[2])
+                    lght[j] = float(line[3])
+                    if date != line[0]:
+                        if i >= (24*4):  # end of day reached
+                            plotDay(date, ts, temp, False)
+                        date = line[0];
+            # plot last day
+            plotDay(date, ts, temp, True)
+
+def plotDay(date, ts, temp, showPlot):
+    plt.plot(ts, temp, label=date)
+    plt.xticks(ts[::8])
+    plt.tick_params(axis='x', labelrotation=90)
+    if showPlot:
+        plt.legend()
+        plt.show()
+
+plotCSV('log.csv')
