f6cd9846d8df363d0c5d51eee91658acfeee83e4
Kristof Van Laerhoven <kryztov@gmail.com>
2024-02-16 16:29:00 +0100

    Update Puckapp.js

--- node/Puckapp.js
+++ node/Puckapp.js
@@ -1,13 +1,17 @@
 const MAX = 96;  // quarter of hours per day, (1440/15)
-const MAX_ARX = 50;  // local storage for past MAX_ARX days
+const MAX_ARX = 10;  // local storage for past MAX_ARX days
 const INV = 900000;  // interval = 15 minutes = 1000*60*60*15
 // day's measurements:
-var data = [ new Int16Array(MAX), new Int16Array(MAX) ];
+var data = [ new Int16Array(MAX), new Int16Array(MAX),
+            new Int16Array(MAX), new Int16Array(MAX),
+            new Int16Array(MAX) ];
 var indx = 0;
 var low_temp = 190;  // alert when below this temperature x10
 var hgh_temp = 220;  // alert when above this temperature x10
 // all measurements:
-var adata = [ new Int16Array(MAX*MAX_ARX), new Int16Array(MAX*MAX_ARX)];
+var adata = [ new Int16Array(MAX*MAX_ARX), new Int16Array(MAX*MAX_ARX),
+            new Int16Array(MAX*MAX_ARX),new Int16Array(MAX*MAX_ARX),
+            new Int16Array(MAX*MAX_ARX)];
 var itr = 0;
 
 function pad2(n) {  // pad an integer to 2 digits
@@ -15,6 +19,9 @@ function pad2(n) {  // pad an integer to 2 digits
 }
 function fPad21(f,s) { // pad a float to ± 2 digits left, 1 right
   return (s?(f<0?'-':'+'):'')+Math.abs(f).toFixed(1).padStart(4,'0');
+}
+function fPad5(n) { // pad an integer to 5 digits
+  return ('0000'+n).substr(-5);
 }
 function toQuart(d) {  // return time d to quarter of hour
   return d.getHours()*4+Math.floor(d.getMinutes()/15);
@@ -25,12 +32,18 @@ function measure() {  // fill in current measurements
   data[0][indx] = E.getTemperature()*10;
   if (typeof(Puck) != "undefined") {
      data[1][indx] = Puck.light()*999;
+     data[2][indx] = Puck.getBatteryPercentage();
+     data[3][indx] = Puck.capSense();
   } else if ((typeof(Bangle) != "undefined")) {
-     data[1][indx] = Bangle.getHealthStatus().steps;
+     data[1][indx] = Bangle.getHealthStatus().bpmConfidence;
+     data[2][indx] = Bangle.getHealthStatus().bpm;
+     data[3][indx] = Bangle.getHealthStatus().steps;
+     data[4][indx] = Bangle.getHealthStatus().movement;
   }
   if (indx==MAX-1) {  // archive at the end of the day
-    for (i=0; i<MAX; i++) adata[0][i+MAX*itr] = data[0][i];
-    for (i=0; i<MAX; i++) adata[1][i+MAX*itr] = data[1][i];
+    for (s=0; s<5; s++)
+        for (i=0; i<MAX; i++) 
+             adata[s][i+MAX*itr] = data[s][i];
     itr++;  if (itr==MAX_ARX) itr = 0;
   }
   NRF.setAdvertising({0x1809 : [Math.round(data[0][indx])] });
@@ -48,12 +61,18 @@ function prnt(day) {  // print out daily view via serial
       if (day==0) {
         outstr += (i==indx)?"[":" ";
         outstr += fPad21(data[0][i]/10,1)+",";
-        outstr += fPad21(data[1][i]/10,0);
+        outstr += fPad21(data[1][i]/10,0)+",";
+        outstr += fPad5(data[2][i],0)+",";
+        outstr += fPad5(data[3][i],0)+",";
+        outstr += fPad5(data[4][i],0);
         outstr += (i==indx)?"]":" ";
       } else {
         day_mem = ((itr-day)<0?MAX_ARX+(itr-day):(itr-day))%MAX_ARX;
         outstr += " "+fPad21(adata[0][i+MAX*day_mem]/10,1)+",";
-        outstr += fPad21(adata[1][i+MAX*day_mem]/10,0)+" ";
+        outstr += fPad21(adata[1][i+MAX*day_mem]/10,0)+",";
+        outstr += fPad5(adata[2][i+MAX*day_mem],0)+",";
+        outstr += fPad5(adata[3][i+MAX*day_mem],0)+",";
+        outstr += fPad5(adata[4][i+MAX*day_mem],0);
       }
     }
     console.log(outstr);
