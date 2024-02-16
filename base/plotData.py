ce0950609a1c0f6ffda464de059804727b8b7a86
Kristof Van Laerhoven <kryztov@gmail.com>
2024-02-16 10:27:14 +0100

    Update plotData.py
    refactoring and scaling up sensors

--- base/plotData.py
+++ base/plotData.py
@@ -6,12 +6,16 @@ import csv
 def plotCSV(logFileName):
     date = ""
     try:
+        # first read file once to see how many lines & dimensions we have:
         file = open(logFileName)
+        numSensors = 2
         readsPerHour = 4
         numDays = int( sum(1 for line in file) / (24*readsPerHour) )
-        ts = [str(x) for x in range(0, 24*readsPerHour)]
-        temp = [x for x in range(0, 24*readsPerHour)]
-        lght = [x for x in range(0, 24*readsPerHour)]
+        ts = [str(x) for x in range(24*readsPerHour)]
+        data = [ [x for x in range(24*readsPerHour)]
+                    for y in range(numSensors) ]
+        fig, axs = plt.subplots(1,numSensors)
+        fig.set_size_inches(10, 5)
         file = open(logFileName)
     except FileNotFoundError:
         print('Error: '+file.name+' not found')
@@ -21,37 +25,47 @@ def plotCSV(logFileName):
             csv_reader = csv.reader(file)
             for i, line in enumerate(csv_reader, 0):
                 j = i % 96
-                if len(line)==4:
+                if len(line)==(numSensors+2):
+                    # a line holds:  0:date, 1:timestamp, 2,...:sensor data
                     ts[j] = line[1];
-                    temp[j] = float(line[2])
-                    lght[j] = float(line[3])
+                    for s in range(numSensors):
+                        data[s][j] = float(line[2+s])
                     if date != line[0]:
                         if i >= (24*readsPerHour):  # end of day reached
                             thisDay = int(i/(24*readsPerHour))-1
-                            plotDay(date, ts, temp, thisDay, numDays)
+                            for s in range(numSensors):
+                                plotDay(axs[s], date, ts, data[s][:],
+                                        thisDay, numDays)
                         date = line[0];
             # plot last day
-            plotDay(date, ts, temp, numDays-1, numDays)
+            for s in range(numSensors):
+                plotDay(axs[s], date, ts, data[s][:], numDays-1, numDays)
+            plotShow(axs, ts)
 
 # plot a single day, use showPlot to display all after the last day
-def plotDay(date, ts, temp, day, numDays):
+def plotDay(ax, date, ts, data, day, numDays):
     greyLvl = 1-((day+1)/numDays)
-    print(greyLvl)
-    plt.plot(ts, temp, label=date, linestyle="-",
+    ax.plot(ts, data, label=date, linestyle="-",
         marker="o", alpha=0.95, markersize=2,
         color=(greyLvl,greyLvl,greyLvl) )
-    plt.xticks(ts[::8])
-    plt.tick_params(axis='x', labelrotation=90)
-    if day+1 == numDays:
-        plt.legend()
-        plt.grid(visible=True, which='major', axis='both')
-        plt.tight_layout()
-        cursor = mplcursors.cursor(hover=mplcursors.HoverMode.Transient)
-        cursor.connect(  # display time and temperature:
-            "add", lambda sel: sel.annotation.set_text(
-                sel.artist.get_label() + "\n" +
-                ts[ int(sel.target[0]) ] + ": " +
-                "{:3.1f}".format(sel.target[1]) + "'C" ) )
-        plt.show()
 
+# finish the plots, add interactivty and a legend
+def plotShow(axes, ts):
+    for ax in axes:
+        ax.set_xticks(ts[::8])
+        ax.tick_params(axis='x', labelrotation=90)
+        ax.grid(visible=True, which='major', axis='both')
+        ax.legend()
+        ax.set_xlim([0,len(ts)-1])
+    plt.tight_layout()
+    # add interactive cursor:
+    cursor = mplcursors.cursor( hover=mplcursors.HoverMode.Transient )
+    cursor.connect(  # display time and temperature:
+        "add", lambda sel: sel.annotation.set_text(
+            sel.artist.get_label() + "\n" +
+            ts[ int(sel.target[0]) ] + ": " +
+            "{:3.1f}".format(sel.target[1]) ) )
+    plt.show()
+
+# start the plotting:
 plotCSV('log.csv')
